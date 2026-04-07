d51d3c7ac1504ecd0aa79aaacf24de8f08dfe947
Nikunj Saini <nikunj902782@gmail.com>
2026-04-07 21:55:22 +0530

    build: Add data acess logic (Createorder, GetOrder)

--- internals/models/order.go
+++ internals/models/order.go
@@ -34,3 +34,38 @@ type Order struct {
 	Items        []OrderItem `gorm:"foreignKey:OrderID" json:"pizzas"`
 	CreatedAt    time.Time   `json:"createdAt"`
 }
+
+type OrderItem struct {
+	ID           string `gorm:"primaryKey;size:14" json:"id"`
+	OrderID      string `gorm:"index;size:14;not null" json:"orderId"`
+	Size         string `gorm:"not null" json:"size"`
+	Pizza        string `gorm:"not null" json:"pizza"`
+	Instructions string `json:"instructions"`
+}
+
+func (o *Order) BeforeCreate(tx *gorm.DB) error {
+	if o.ID == "" {
+		o.ID = shortid.MustGenerate()
+	}
+
+	return nil
+}
+
+func (oi *OrderItem) BeforeCreate(tx *gorm.DB) error {
+	if oi.ID == "" {
+		oi.ID = shortid.MustGenerate()
+	}
+
+	return nil
+}
+
+func (o *OrderModel) CreateOrder(order *Order) error {
+	return o.DB.Create(order).Error
+}
+
+func (o *OrderModel) GetOrder(id string) (*Order, error) {
+	var order Order
+	err := o.DB.Preload("Items").First(&order, "id = ?", id).Error
+	return &order, err
+}
+
